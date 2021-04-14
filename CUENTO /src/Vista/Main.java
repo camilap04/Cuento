a331a9416d54eed765bd0cb8df673a964ee6b346
Camila Paredes <mariacamilaparedes@gmail.com>
2021-04-13 21:26:13 -0500

    Se hace un mousePressed

--- CUENTO /src/Vista/Main.java
+++ CUENTO /src/Vista/Main.java
@@ -1,5 +1,100 @@
 package Vista;
 
-public class Main {
+import processing.core.PImage;
+import processing.core.PApplet;
 
+public class Main extends PApplet {
+
+	PImage ARBOLES;
+	PImage CANASTA;
+	PImage CAPERUCITA;
+	PImage FONDO;
+	PImage HONGO;
+	PImage LOBO;
+	PImage PANTALLA1;
+	PImage PANTALLADOS;
+	PImage ULTIMAPANTALLA;
+
+	int screen;
+
+	public static void main(String[] args) {
+
+		String[] processingArgs = {"Main"};
+		Main mySketch = new Main();
+		PApplet.runSketch(processingArgs, mySketch);
+
+	} // MAIN
+
+
+	public void settings () {
+		size (1152,700);
+
+	} // SETTINGS
+
+	public void setup () {
+
+		ARBOLES = loadImage ("IMG/ARBOLES.png");
+		CANASTA = loadImage ("IMG/CANASTA.png");
+		CAPERUCITA = loadImage ("IMG/Caperucita.png");
+		FONDO = loadImage ("IMG/FONDO.png");
+		HONGO = loadImage ("IMG/HONGO.png");
+		LOBO = loadImage ("IMG/LOBO.png");
+		PANTALLA1 = loadImage ("IMG/PANTALLA1.png");
+		PANTALLADOS = loadImage ("IMG/PANTALLADOS.png");
+		ULTIMAPANTALLA = loadImage ("IMG/ULTIMAPANTALLA.png");
+
+		screen = 1;
+	}
+
+	public void draw () {
+		switch (screen) {
+		case 1:
+			image (PANTALLA1, 0,0);
+
+			break;
+		case 2:
+			image (PANTALLADOS, 0,0);
+
+			break;
+		case 3:
+			image (FONDO, 0,0);
+
+			break;
+
+		case 4:
+			image (ULTIMAPANTALLA, 0,0);
+
+			break;
+
+		default:
+			break;
+		}		
+
+
+	}
+
+	public void mousePressed () {
+		
+		switch (screen) {
+		case 1:
+		
+			break;
+		case 2:
+		
+
+			break;
+		case 3:
+		
+			break;
+
+		case 4:
+	
+
+			break;
+
+		default:
+			break;
+		}		
+
+	}
 }
