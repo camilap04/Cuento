5a39f950642b43aa09b7da48aa05c09a84f5014e
Camila Paredes <mariacamilaparedes@gmail.com>
2021-04-14 04:42:09 -0500

    mousepressed para devolverse entre pantallas

--- CUENTO /src/Vista/Main.java
+++ CUENTO /src/Vista/Main.java
@@ -14,9 +14,12 @@ public class Main extends PApplet {
 	PImage PANTALLA1;
 	PImage PANTALLADOS;
 	PImage ULTIMAPANTALLA;
+	PImage PIEDRA;
 
 	int screen;
-
+	
+	boolean loboClic;
+	
 	public static void main(String[] args) {
 
 		String[] processingArgs = {"Main"};
@@ -42,6 +45,7 @@ public class Main extends PApplet {
 		PANTALLA1 = loadImage ("IMG/PANTALLA1.png");
 		PANTALLADOS = loadImage ("IMG/PANTALLADOS.png");
 		ULTIMAPANTALLA = loadImage ("IMG/ULTIMAPANTALLA.png");
+		PIEDRA = loadImage ("IMG/PIEDRA.png");
 
 		screen = 1;
 	}
@@ -52,6 +56,8 @@ public class Main extends PApplet {
 			image (PANTALLA1, 0,0);
 
 			break;
+			
+			
 		case 2:
 			image (PANTALLADOS, 0,0);
 
@@ -63,13 +69,30 @@ public class Main extends PApplet {
 			image (LOBO, 0,0);
 			image (HONGO, 0,0);
 			image (CANASTA, 0,0);
+			image (PIEDRA, 0,0);
 
 			break;
 
 		case 4:
 			image (ULTIMAPANTALLA, 0,0);
+			
+		case 5:
+			image (PANTALLADOS, 0,0);
 
 			break;
+			
+			
+			case 6:
+				image (PANTALLA1, 0,0);
+
+				break;
+				
+			case 7 :
+				image (PANTALLADOS, 0,0);
+
+				break;
+
+		
 
 		default:
 			break;
@@ -85,8 +108,8 @@ public class Main extends PApplet {
 		switch (screen) {
 		case 1:
 			
-			if (mouseX > 740 && mouseX < 981
-					&& mouseY > 390 && mouseY < 429) //CONTINUE
+			if (mouseX > 762 && mouseX < 967
+					&& mouseY > 408 && mouseY < 449) //CONTINUE
 				screen = 2;
 			
 			
@@ -94,20 +117,58 @@ public class Main extends PApplet {
 		case 2:
 			
 		
-			if (mouseX > 834 && mouseX < 1076
-					&& mouseY > 602 && mouseY < 646) //CONTINUE
+			if (mouseX > 954 && mouseX < 1105
+					&& mouseY > 632&& mouseY < 660) //CONTINUE
 				screen = 3;
+			
+			case 3:
+			
+			
+			if (mouseX > 24 && mouseX < 112
+					&& mouseY > 625 && mouseY < 672) // Para devolverme de la segunda pantalla a la primera
+				screen = 1;
 		
+			
+			case 4:
+			
+			
+			if (mouseX > 24 && mouseX < 112
+					&& mouseY > 625 && mouseY < 672) // Para devolverme de la TERCER pantalla a la segunda
+				screen = 2;
 
 			break;
-		case 3:
+		case 5:
 		
-			break;
+			if (mouseX > 38 && mouseX < 124
+					&& mouseY > 624 && mouseY < 668) // caperucita roja
+				
+				break;
+		
+			println("Caperucita");	
+			
+	
+			
+			if (mouseX > 422 && mouseX < 565
+					&& mouseY > 97 && mouseY < 261) // lobo
+				println("lobo");
+			
+			if (mouseX > 640 && mouseX < 733
+					&& mouseY > 569 && mouseY < 689) // canasta
+				println("canasta");
+			
+			if (mouseX > 848 && mouseX < 967
+					&& mouseY > 584 && mouseY < 676) // hongo
+				println("hongo");
+			
+			if (mouseX > 654 && mouseX < 801
+					&& mouseY > 367 && mouseY < 472) // piedra
+				println("Piedra");
+				break;
 
-		case 4:
+
 	
 
-			break;
+			
 
 		default:
 			break;
