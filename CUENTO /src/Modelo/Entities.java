a44df553a107ee06ae71d18c3c6f253dd2abb940
Camila Paredes <mariacamilaparedes@gmail.com>
2021-04-14 19:35:41 -0500

    se cargar el txt

--- CUENTO /src/Modelo/Entities.java
+++ CUENTO /src/Modelo/Entities.java
@@ -6,25 +6,31 @@ import java.io.FileReader;
 public  class Entities {
 
 	public void loadtext () {
-	try {
-	FileReader fr = new FileReader ("Data/datos.txt");
-	
-	BufferedReader br = new BufferedReader (fr);
-	
-	String strCurrentLine;
-	while((strCurrentLine= br.readLine())!= null) {
-		System.out.println(strCurrentLine);
-	
+		try {
+			FileReader fr = new FileReader ("Data/datos.txt");
+
+			BufferedReader br = new BufferedReader (fr);
+
+			String strCurrentLine;
+			while((strCurrentLine= br.readLine())!= null) {
+				System.out.println(strCurrentLine);
+
+			}
+
+
+		}
+
+		catch (Exception e) {}
+
+
+
 	}
 	
+	public static void main(String[] args) {
+	Entities e = new Entities();
+	e.loadtext();
+	
 	
 	}
-	
-	catch (Exception e) {}
-	
-			
-		
-	}
-	
-	
+
 }
